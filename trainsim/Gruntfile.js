1705c3a8ab9599e1cff21765a2daebd18bf474b4
OrionShah <dronshv@yandex.ru>
2015-12-01 22:18:45 +0300

    Настройка сборка под Винду

--- trainsim/Gruntfile.js
+++ trainsim/Gruntfile.js
@@ -10,19 +10,10 @@ module.exports = function(grunt) {
         }
       }
     },
-    nodewebkit: {
-      options: {
-        version: 'v0.12.0',
-        platforms: ['linux64'],
-        // platforms: ['win', 'osx', 'linux32', 'linux64'], // Платформы, под которые будет строиться наше приложение
-        buildDir: './build', // Путь, по которому будет располагаться построенное приложение
-      },
-      src: './app/**/*' // Путь, по которому располагаются исходные коды приложения
-    },
     nwjs: {
       options: {
         version: 'v0.12.0',
-        platforms: ['linux64'],
+        platforms: ['linux64', 'win64'],
         buildDir: './build', // Where the build version of my NW.js app is saved
       },
       src: ['./app/**/*'] // Your NW.js app
@@ -31,7 +22,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-node-webkit-builder');
   grunt.loadNpmTasks('grunt-nw-builder');
   grunt.registerTask('default', ['watch']);
 };
